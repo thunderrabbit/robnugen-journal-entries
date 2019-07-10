5e8f1ca355844b4bc211204dc3e9264a4a6ac208
Rob Nugen <rob@robnugen.com>
2019-07-10 12:48:16 +0900

    through

--- upblogpic.sh
+++ upblogpic.sh
@@ -2,8 +2,8 @@
 # This is designed to make it easier to copy images from finder to b.robnugen.com
 
 echo remember you can
-echo ssh b.rn 'mkdir -p ~/b.robnugen.com/journal/2020'
+echo ssh b.rn 'mkdir -p ~/b.robnugen.com/blog/2019'
 
-scp $@ b.rn:b.robnugen.com/journal/2019
+scp $@ b.rn:b.robnugen.com/blog/2019
 
 
