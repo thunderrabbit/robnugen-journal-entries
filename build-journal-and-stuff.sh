c7c3ac169f36fba21a572882e5e0f929f418975b
Rob Nugen <rob@robnugen.com>
2019-09-26 00:19:23 +0900

    After

--- build-journal-and-stuff.sh
+++ build-journal-and-stuff.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+
+if [ -z "$1" ]
+  then
+    echo "Usage: $0 <commit message for main site>"
+    exit
+fi
+
+COMMIT_MESSAGE=$@
+
+echo "Will use \"${COMMIT_MESSAGE}\" as the commit message"
+
 echo "pushing"
 git push bitbucket master
 git push norigin master &
@@ -16,8 +27,9 @@ git push origin master
 git push gh master
 
 cd ~/journal-hugo/
-git add .
-git commit -m "latest entries"
+git add content/journal/
+git commit -m "$COMMIT_MESSAGE"
+exit
 git push bb master
 git push origin master
 git push origin netlify
