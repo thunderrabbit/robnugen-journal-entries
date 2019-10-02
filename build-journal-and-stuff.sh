57087187d13c5b5e6f10322d182d1c92ec08429e
Rob Nugen <rob@robnugen.com>
2019-10-03 00:39:21 +0900

    amusement

--- build-journal-and-stuff.sh
+++ build-journal-and-stuff.sh
@@ -15,10 +15,6 @@ git push bitbucket master
 git push norigin master &
 git push github master &
 
-echo "not pulling"
-# cd ~/journal-hugo/
-# git submodule foreach git pull
-
 cd ~/journal-hugo/content/journal
 git pull
 
@@ -28,26 +24,3 @@ git commit -m "$COMMIT_MESSAGE"
 git push bb master
 git push origin master
 git push origin netlify
-
-echo "building"
-hugo
-
-echo "deploying"
-echo "changing directory"
-cd public
-echo "sleeping"
-sleep 2
-echo "adding"
-git add .
-echo "sleeping"
-sleep 2
-echo "committing"
-git commit -m "Published on `date`"
-echo "sleeping"
-sleep 2
-echo "pushing"
-git push origin master
-
-# cd ~/journal
-# echo skipping ./g2f.sh
-# echo because it needs the password which I do not have handy
