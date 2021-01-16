deeff18cc74ce5c19767002ca33ab2f42921ec2a
Rob Nugen <rob@robnugen.com>
2021-01-17 00:38:02 +0900

    　

--- transcribe_weekly_alignments.sh
+++ transcribe_weekly_alignments.sh
@@ -36,6 +36,8 @@ echo using audio $AUDIO
 
 filename_no_ext="$AUDIO"
 filename_no_ext=${filename_no_ext%.mp3}    # % = remove .mp3 if we sent a .mp3 file
+filename_no_ext=${filename_no_ext%.wav}    # % = remove .wav if we sent a .wav file
+filename_no_ext=${filename_no_ext%.ogg}    # % = remove .ogg if we sent a .ogg file
 
 
 ffmpeg -r 1 -loop 1 -y -i $IMAGE -i $AUDIO -acodec mp3 -r 1 -shortest $filename_no_ext.flv
