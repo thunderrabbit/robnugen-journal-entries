dfd446ccabef66789fbc54c1bdc7a13b05a8eb24
Rob Nugen <rob@robnugen.com>
2024-12-27 17:44:46 +0900

    amazing.

--- build_up_local_journal_quantity.sh
+++ build_up_local_journal_quantity.sh
@@ -3,20 +3,11 @@
 
 ####   BEGIN get files from ~/barefoot_rob_master/content/journal
 
-REMOTE_UNTRACKED_FILES=$(~/journal/untracked_remote_journal_file_shower.sh)
+LATEST_REMOTE_BRANCH=$(~/journal/untracked_remote_journal_file_shower.sh)
 
-echo $REMOTE_UNTRACKED_FILES
-
-if [ -n "$REMOTE_UNTRACKED_FILES" ]   # check non-zero length
-    then
-	echo "Here are untracked remote files I'll get."
-        while read -r line; do
-              echo "$line"
-        done <<< "$REMOTE_UNTRACKED_FILES"
-
-        echo Calling script to get the files, then exiting
-	~/journal/untracked_remote_journal_file_getter.sh "$REMOTE_UNTRACKED_FILES"
-fi
+# Given the branch name, checkout that branch locally
+git pull
+git checkout $LATEST_REMOTE_BRANCH
 
 ####   END get files from ~/barefoot_rob_master/content/journal
 
--- untracked_remote_journal_file_shower.sh
+++ untracked_remote_journal_file_shower.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-# This is designed to show what files exist on my journal Micropub server endpoint but not included in the journal repo
-#
-# 3 January 2021
-#
-# -F specifies a config file that does not show the ssh key visual fingerprint
-# bfr is my journal Micropub server endpoint defined in .ssh/config_no_visual_keys
-# git ls-files -o  shows untracked files on a line per line basis but does not handle spaces in filesnames  https://stackoverflow.com/a/8506155/194309
+# This is designed to show the name of the branch that HEAD is pointing to on the remote server.
 
 # REMOTE_JOURNAL_DIR should match ~/journal/untracked_remote_journal_file_getter.sh
 REMOTE_JOURNAL_DIR='~/barefoot_rob_journal/content/journal'    #REMOTE_JOURNAL_DIR must be in single quotes so ~ does not expand locally.
 
+# Execute git log command and capture the output
+ssh -F ~/.ssh/config_no_visual_keys bfr "cd $REMOTE_JOURNAL_DIR; git log --oneline --decorate -n 1" > branch_info.txt
 
-ssh -F ~/.ssh/config_no_visual_keys bfr "cd $REMOTE_JOURNAL_DIR; git ls-files -o"
+# Extract the branch name from the commit message
+BRANCH_NAME=$(grep -oP '\((HEAD -> )?(\w+)' < branch_info.txt | sed 's/HEAD -> //')
+
+# Remove the extra open parenthesis if present
+BRANCH_NAME=${BRANCH_NAME#(}
+
+echo $BRANCH_NAME
